38d95ddffd5bc6a54b1df6548ff83090434bfe32
Mel <brandon.melend14@gmail.com>
2022-09-27 16:05:52 -0400

    Added new modes and updated ui

--- sketch-logic.js
+++ sketch-logic.js
@@ -2,17 +2,19 @@ let drawColor = 'black'
 let colorModeCheck = true;
 let rgbModeCheck = false; 
 let shadowModeCheck = false; 
+let clearGridCheck = false; 
+
 function drawOn(e){
     const gridSquare = document.querySelector(`#${e.target.id}`)
+
     if(rgbModeCheck){
-       
         let newColor = genRandomColor();
         gridSquare.setAttribute('style', `background-color: rgb(${newColor[0]},${newColor[1]},${newColor[2]},1)`);
     }else if(shadowModeCheck){
 
         console.log(getComputedStyle(gridSquare).getPropertyValue("background-color"));
             let currentVisibility = parseFloat(getComputedStyle(gridSquare).getPropertyValue('background-color').split(',')[3]);
-            console.log(currentVisibility)
+            
             if(currentVisibility <= 0.9){
                 gridSquare.setAttribute('style',`background-color:rgb(0,0,0,${currentVisibility+=0.1})`);
             }else{ 
@@ -28,11 +30,12 @@ function drawOn(e){
 
 function resizeGrid(e){
     const newGrid = document.querySelector("#myRange");
-
+    
     const gridItems = document.querySelectorAll('.grid-item');
     gridItems.forEach(square => square.remove())
     createGrid(newGrid.value,newGrid.value);
 }
+
 function setColor(e){
     const newColor = document.querySelector('#colorChoice');
     drawColor = newColor.value
@@ -49,12 +52,20 @@ function setColorMode(e){
     colorModeCheck = true; 
     rgbModeCheck = false;
     shadowModeCheck = false; 
+
+    clearButtonClass();
+    const colorButton = document.querySelector('#color-mode');
+    colorButton.classList.toggle('button-active');
 }
 
 function setRGBMode(e){
     colorModeCheck = false; 
     rgbModeCheck = true;
     shadowModeCheck = false; 
+
+    clearButtonClass();
+    const rgbButton = document.querySelector('#RGB-mode');
+    rgbButton.classList.toggle('button-active');
 }
 
 
@@ -62,9 +73,36 @@ function setShadowMode(e){
     colorModeCheck = false; 
     rgbModeCheck = false;
     shadowModeCheck = true; 
+
+    clearButtonClass();
+    const shadowButton = document.querySelector('#shadow-mode');
+    shadowButton.classList.toggle('button-active');
 }
 
 
+
+function clearGridSquares(e){
+
+    const gridSquares = document.querySelectorAll('.grid-item');
+    gridSquares.forEach(square => square.setAttribute('style',`background-color:rgb(255,255,255,0)`));
+
+    const clearButton = document.querySelector('#clear-mode')
+    clearButton.classList.add('button-active');
+    setTimeout(() => {
+        clearButton.classList.remove('button-active');
+    }, 700);
+    
+
+}
+
+function clearButtonClass(){
+    const buttonOptions = document.querySelectorAll('.option-button');
+    buttonOptions.forEach(option => {
+        if(option.classList.contains('button-active')){
+            option.classList.remove('button-active');
+        }
+    });
+}
 const sketchContainer = document.querySelector(".sketch-container");
 function createGrid(col,row){
    sketchContainer.style.setProperty('--grid-cols',col);
@@ -94,5 +132,16 @@ rgbMode.addEventListener('click',setRGBMode);
 const shadowMode = document.querySelector('#shadow-mode');
 shadowMode.addEventListener('click',setShadowMode);
 
+const clearGrid = document.querySelector('#clear-mode');
+clearGrid.addEventListener('click',clearGridSquares);
+
+
 const colorPicker = document.querySelector('#colorChoice');
 colorPicker.addEventListener('change',setColor);
+
+const rangeContainer = document.querySelector('.range-container');
+
+rangeContainer.textContent =  `Grid Size: ${resizeRange.value} x ${resizeRange.value}`;
+resizeRange.oninput = function(){
+    rangeContainer.textContent = `Grid Size: ${this.value} x ${this.value}`;
+}
